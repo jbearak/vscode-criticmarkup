09bef59b38390d31eaae1c69a007257399f2583e
Jonathan Marc Bearak <jbearak@guttmacher.org>
2025-12-04 11:29:29 -0500

    Restore source files and fix build configuration
    - Restored src/ folder with TypeScript source files from git history
    - Fixed .gitignore to ignore out/ instead of src/
    - Removed @types/bun package (incompatible with VS Code extension)
    - Updated TypeScript from 3.3 to 5.9 for modern syntax support
    - Removed compiled out/ folder from git tracking
    - Added Bun configuration (bun.lock, AGENTS.md)
    - Removed package-lock.json in favor of bun.lock

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,4 +1,3 @@
-"use strict";
 //
 // PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
 //
@@ -10,14 +9,15 @@
 // host can call to run the tests. The test runner is expected to use console.log
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
-Object.defineProperty(exports, "__esModule", { value: true });
-const testRunner = require("vscode/lib/testrunner");
+
+import * as testRunner from 'vscode/lib/testrunner';
+
 // You can directly control Mocha options by configuring the test runner below
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options
 // for more info
 testRunner.configure({
-    ui: 'tdd',
+    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
     useColors: true // colored output from test results
 });
+
 module.exports = testRunner;
-//# sourceMappingURL=index.js.map
